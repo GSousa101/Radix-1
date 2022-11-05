13f177fc52ff349b99b857ef71e042f6cd9a7bc8
Gustavo F Sousa <60138973+GSousa101@users.noreply.github.com>
2022-11-05 05:19:12 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,7 @@ int	main(void)
 	//verificar qual unidade esta precisado de recursos
 	//verificar qual porto consegue fornecer
 	//verificar a melhor rota de navios para saciar todas as necessidades
+	//daria pra jogar tudo pra js, assim o usuario poderia acessar todas as informações pelo site, centralizando e sendo mais facil a analise.
+	//isso ja ajudaria mesmo sem o algoritmo fazendo o trabalho
 	return (0);
 }
